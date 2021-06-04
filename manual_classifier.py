ab5e493fd5ea3ad7943290ce869891c624f7f66a
Sebastian Waisbrot <seppo0010@gmail.com>
2021-06-04 07:28:25 -0300

    get train data

--- manual_classifier.py
+++ manual_classifier.py
@@ -4,9 +4,9 @@ import cv2
 from compare_images import compare_images
 import urllib
 
-app = Flask(__name__, static_folder='to_classify')
-template_path = 'template'
 toclassify_path = 'to_classify'
+app = Flask(__name__, static_folder=toclassify_path)
+template_path = 'template'
 target_path = 'train'
 templates = [(f, cv2.imread(os.path.join(template_path, f))) for f in os.listdir(template_path) if os.path.isfile(os.path.join(template_path, f))]
 
@@ -22,19 +22,16 @@ def index():
     similarities = [(template_name, get_score(template_image)) for (template_name, template_image) in templates]
     similarities = sorted(similarities, key=lambda s: -s[1])
     q = urllib.parse.quote
-    return f'<img src="/to_classify/{path}" height=600 /><br><a href="classify?image={q(path)}&template=">None</a><br>' + '<br>'.join(f'<a href="classify?image={q(path)}&template={q(s[0])}">{s[0]}</a>' for s in similarities)
+    return f'<img src="/{os.path.basename(toclassify_path)}/{path}" height=600 /><br><a href="classify?image={q(path)}&template=">None</a><br>' + '<br>'.join(f'<a href="classify?image={q(path)}&template={q(s[0])}">{s[0]}</a>' for s in similarities)
 
 @app.route("/classify")
 def classify():
     image = request.args['image']
-    template = request.args['template']
+    template = request.args['template'] or 'garbage'
     source = os.path.join(toclassify_path, image)
-    if template == '':
-        os.remove(source)
-    else:
-        if not os.path.isdir(os.path.join(target_path, template)):
-            os.mkdir(os.path.join(target_path, template))
-        os.rename(source, os.path.join(target_path, template, image))
+    if not os.path.isdir(os.path.join(target_path, template)):
+        os.mkdir(os.path.join(target_path, template))
+    os.rename(source, os.path.join(target_path, template, image))
     return redirect('/')
 
 if __name__ == '__main__':
