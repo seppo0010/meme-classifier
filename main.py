844c10db0ee777464acfdebc6560ea1b101fe8e1
Sebastian Waisbrot <seppo0010@gmail.com>
2021-06-03 22:34:28 -0300

    refactor

--- main.py
+++ main.py
@@ -1,51 +1,11 @@
-from skimage.metrics import structural_similarity, normalized_root_mse
 import cv2
-import numpy as np
 import sys
+from compare_images import compare_images
 
-print(sys.argv[1], sys.argv[2])
-before = cv2.imread(sys.argv[1])
-after = cv2.resize(cv2.imread(sys.argv[2]), [before.shape[1], before.shape[0]])
-
-# Convert images to grayscale
-before_gray = cv2.cvtColor(before, cv2.COLOR_BGR2GRAY)
-after_gray = cv2.cvtColor(after, cv2.COLOR_BGR2GRAY)
-
-# Compute SSIM between two images
-(score, diff) = structural_similarity(before_gray, after_gray, full=True)
-print("Image similarity", score)
-
-# The diff image contains the actual image differences between the two images
-# and is represented as a floating point data type in the range [0,1]
-# so we must convert the array to 8-bit unsigned integers in the range
-# [0,255] before we can use it with OpenCV
-diff = (diff * 255).astype("uint8")
-
-# Threshold the difference image, followed by finding contours to
-# obtain the regions of the two input images that differ
-thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-contours = contours[0] if len(contours) == 2 else contours[1]
-
-mask = np.zeros(before.shape, dtype='uint8')
-filled_after = after.copy()
-
-for c in contours:
-    area = cv2.contourArea(c)
-    if area > 40:
-        x,y,w,h = cv2.boundingRect(c)
-        cv2.rectangle(before, (x, y), (x + w, y + h), (36,255,12), 2)
-        cv2.rectangle(after, (x, y), (x + w, y + h), (36,255,12), 2)
-        cv2.drawContours(mask, [c], 0, (0,255,0), -1)
-        cv2.drawContours(filled_after, [c], 0, (0,255,0), -1)
-
-# hist_before = cv2.calcHist(before, [1], None, [256], [0, 256])
-# hist_after = cv2.calcHist(after, [1], None, [256], [0, 256])
-# print(hist_before, hist_after)
-print('root mse', normalized_root_mse(before, after))
-# cv2.imshow('before', before)
-# cv2.imshow('after', after)
-# cv2.imshow('diff',diff)
-# cv2.imshow('mask',mask)
-# cv2.imshow('filled after',filled_after)
-# cv2.waitKey(0)
+path_before = sys.argv[1]
+before = cv2.imread(path_before)
+for path_after in sys.argv[2:]:
+    after = cv2.imread(path_after)
+    res = compare_images(before, after)
+    res2 = compare_images(after, before)
+    print(f'{path_before},{path_after},{res["similarity"]+res2["similarity"]},{res["mse"]+res2["mse"]}')
