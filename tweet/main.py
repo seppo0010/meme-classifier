ae2d88cc3bd01a4d31489b289ff886fe854d885b
Mariano Vicente <m.vicent@gmail.com>
2022-02-06 23:38:54 -0300

    Darle oportunidad a otros memes.
    Por falta de actividad se pierden memes solo por un pequeño threshold.

--- tweet/main.py
+++ tweet/main.py
@@ -37,7 +37,7 @@ async def main():
     cur = conn.cursor()
     cur.execute(
     '''
-    SELECT chat_id, message_id FROM meme WHERE date > %s AND upvotes >= 5 ORDER BY upvotes - downvotes DESC
+    SELECT chat_id, message_id FROM meme WHERE date > %s AND upvotes >= 4 ORDER BY upvotes - downvotes DESC
     ''', [date])
     res = cur.fetchone()
     if res is None:
